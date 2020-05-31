3232566b074808c4b9493b80d973fb045eae1432
Gary Lai <garylai1990@gmail.com>
2020-06-01 00:02:29 +0800

    :white_check_mark: (test) fix typo

--- test/StockPrices/PriceOnly.test.ts
+++ test/StockPrices/PriceOnly.test.ts
@@ -1,6 +1,6 @@
 import { price } from "../../src/StockPrices";
 
-test("Stock Prices #test", async () => {
+test("Stock Prices #price", async () => {
   const symbol = "TWTR";
   const data = await price(symbol.toLowerCase());
   expect(typeof data === "number").toBe(true);
